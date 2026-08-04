251102399d504c5f51c4556e39f0effc1c8fb96e
isaac <isclopez2016@gmail.com>
2026-08-03 20:28:43 -0500

    Added Assignment Model and register in admin

--- summer-project2026/project/webapp/models.py
+++ summer-project2026/project/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Assignment(models.Model):
+    title = models.CharField(max_length=200)
+    class_name = models.CharField(max_length=100)
+    due_date = models.DateTimeField()
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.class_name})"
